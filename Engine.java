b5b2faffc7a43523de1392c21cf705b4b3e07c85
Kalinin32 <vadim-kalinin-2020@mail.ru>
2025-03-20 22:39:08 +0600

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,18 @@
+package Probe;
+
+public class Engine {
+    public Engine(){
+    }
+    public Engine(double volume){
+        this.volume=volume;
+    }
+    private double volume;
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+}
