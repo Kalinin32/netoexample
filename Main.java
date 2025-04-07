40a83998a9a34a9db24338e36a51ea6f9bed40b8
Kalinin32 <vadim-kalinin-2020@mail.ru>
2025-04-08 01:02:41 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,57 @@
 import java.util.Scanner;
 public class Main {
-    public static int YearDays(int year){
-        if((year%400==0)||(year%100!=0 && year%4==0)){
-            return 366;
-        }else {
-            return 365;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int earnings = 0;    // доходы
+        int spendings = 0;   // расходы
+        while (true) {
+            System.out.println("1. Добавить новый доход");
+            System.out.println("2. Добавить новый расход");
+            System.out.println("3. Выбрать систему налогообложения");
+            String input =scanner.nextLine();
+            if(input.equals("end")){
+                System.out.println("Программа завершена!");
+                break;
+                }
+            int operation=Integer.parseInt(input);
+            switch (operation){
+                case 1:
+                    System.out.println("Введите сумму дохода:");
+                    String incomE=scanner.nextLine();
+                    int income=Integer.parseInt(incomE);
+                    earnings+=income;
+                    break;
+                case 2:
+                    System.out.println("Введите сумму расхода:");
+                    String expenseS=scanner.nextLine();
+                    int expenses=Integer.parseInt(expenseS);
+                    spendings+=expenses;
+                    break;
+                case 3:
+                    System.out.println("Мы советуем вам УСН доходы");
+                    System.out.println("Ваш налог составит: "+SpendingsEarnings(earnings)+" рублей");
+                    System.out.println("Налог на другой системе: "+EarningsSpendings(earnings,spendings)+" рублей");
+                    System.out.println("Экономия: "+(EarningsSpendings(earnings,spendings)-SpendingsEarnings(earnings))+" рублей");
+                    break;
+                default:
+                    System.out.println("Такой операции нет");
+            }
         }
     }
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Введите год в формате уууу: ");
-        int year=scanner.nextInt();
-        int days=YearDays(year);
-        System.out.println("Количество дней: "+days);
-
-   }
+    public static int EarningsSpendings(int earnings,int spendings){
+        int num1=(earnings-spendings)*15/100;
+        if(num1>=0){
+            return num1;
+        }else {
+            return 0;
+        }
+    }
+    public static int SpendingsEarnings(int earnings){
+        int num2=earnings*6/100;
+        if(num2>=0){
+            return num2;
+        }else {
+            return 0;
+        }
+    }
 }
